cc4dd4b4db14036d08c2afa7339092417a243b1f
Edenn Touitou <edenn.touitou@letsjam.fr>
2024-07-18 17:10:30 +0200

    back of all semiboss

--- src/lib/models/character/enemies/semiboss/index.ts
+++ src/lib/models/character/enemies/semiboss/index.ts
@@ -3,4 +3,4 @@ import boar from './boar.json';
 import kobold from './kobold.json';
 import zombie from './zombie.json';
 
-export const characters: Array<object> = [vampireBat];
+export const characters: Array<object> = [vampireBat, boar, kobold, zombie];
